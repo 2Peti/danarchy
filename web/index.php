4d27159b1e86567e1ae4f89f88e534f4db011f6c
2Peti <pney0807@gmail.com>
2020-05-11 09:53:33 +0200

    first

---
+++ web/index.php
@@ -0,0 +1,74 @@
+<html lang="sk">
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-165933585-1"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	
+	gtag('config', 'UA-165933585-1');
+	</script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Co Chces</title>
+	<link rel=icon href="/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,600,600italic,700,700italic|Comfortaa:400,700" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+
+<!-- banner start -->
+<div class="wrapper wrapper-bg-banner wrapper-center-block banner" data-lead-id="banner">
+	<div class="bg-wrapper">
+        <img src="img/banner.jpg" alt="" data-lead-id="banner-bg">
+    </div>
+    <div class="bg-text middle">
+        <div class="fill">
+    		<div class="container">
+                <div class="row">
+    				<div class="col-xs-12 col-md-11 col-lg-10 center-block text-center">
+                        <div class="inner">
+                            <a href="https://cochces.herokuapp.com/" class="logo" data-lead-id="logo"><img src="img/logo.png" alt=""></a>
+                        </div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+</div>
+<!-- banner ends -->
+
+<div id="section1" class="wrapper wrapper-center-block links" data-lead-id="links">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-xs-12 col-md-11 col-lg-10 center-block">
+                <h2 data-lead-id="links-title"><strong><span style="color:#8afffc">&lt;</span> Links <span style="color:#8afffc">/&gt;</span></strong></h2>
+				<a href="/games" class="btn">Piskvorky</a>
+				<a href="/ip" class="btn">What is my IP</a>
+				<a href="/gps" class="btn">What is my location</a>
+				<a href="/Download" class="btn">Downloads</a>
+				<a href="https://zsrozkn.edupage.org/" class="btn">Edupage</a>
+				<a href="https://www.bezkriedy.sk/metodici.php" class="btn">Bezkriedy</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<footer class="wrapper wrapper-center-block footer" data-lead-id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-11 col-lg-10 center-block text-center">
+                <a href="https://cochces.herokuapp.com/" class="footer-logo" data-lead-id="footer-logo"><img src="img/logo.png" alt=""></a>
+                
+                
+            </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
