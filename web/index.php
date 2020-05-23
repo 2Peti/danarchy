993b55505a46c98e389093d9306f00e2fcb3d074
2Peti <pney0807@gmail.com>
2020-05-23 12:56:12 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -18,47 +18,6 @@
 	<link rel="stylesheet" href="/css/bootstrap.min.css">
 	<link rel="stylesheet" href="/css/style.css">
 
-    <style>
-.tooltip {
-  position: relative;
-  display: inline-block;
-}
-
-.tooltip .tooltiptext {
-  visibility: hidden;
-  width: 140px;
-  background-color: #555;
-  color: #fff;
-  text-align: center;
-  border-radius: 6px;
-  padding: 5px;
-  position: absolute;
-  z-index: 1;
-  bottom: 150%;
-  left: 50%;
-  margin-left: -75px;
-  opacity: 0;
-  transition: opacity 0.3s;
-}
-
-.tooltip .tooltiptext::after {
-  content: "";
-  position: absolute;
-  top: 100%;
-  left: 50%;
-  margin-left: -5px;
-  border-width: 5px;
-  border-style: solid;
-  border-color: #555 transparent transparent transparent;
-}
-
-.tooltip:hover .tooltiptext {
-  visibility: visible;
-  opacity: 1;
-}
-</style>
-
-
 </head>
 <body>
 
@@ -91,9 +50,8 @@ function copy() {
     textArea.select();
     document.execCommand("Copy");
     textArea.remove();
-
-    var tooltip = document.getElementById("myTooltip");
-    tooltip.innerHTML = "Copied!";
+ 
+    alert("Copied!");
 }
 </script>
 <div id="section1" class="wrapper wrapper-center-block ip" data-lead-id="ip">
